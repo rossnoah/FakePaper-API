4319db375ca783f3ad4f61d80b236a7136e982b4
Noah Ross <me@noah.dev>
2024-07-28 08:38:58 -0700

    fix message thing

--- index.ts
+++ index.ts
@@ -101,14 +101,14 @@ function extractTitleFromLatex(latexString: string): string {
 
 // Rate limiter to 2 requests per minute and 10 requests per 24 hours
 const shortTermLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 15 minutes
-  max: 2, // Limit each IP to 100 requests per windowMs
-  message: "Too many requests from this IP, please try again after 15 minutes",
+  windowMs: 1 * 60 * 1000, // 2 minutes
+  max: 2,
+  message: "Too many requests from this IP, please try again after 2 minutes",
 });
 
 const longTermLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hours
-  max: 10, // Limit each IP to 10 requests per windowMs
+  max: 10,
   message: "Too many requests from this IP, please try again after 24 hours",
 });
 
